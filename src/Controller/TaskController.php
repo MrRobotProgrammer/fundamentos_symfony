e6d78ebcbcb11f8c657c65fa247fd6ce9a7c7967
Flavio Santos <flavio.santos@pravaler.com.br>
2019-11-20 17:12:55 -0300

    Tratando submissão do formulário

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,16 +43,17 @@ class TaskController extends AbstractController
      */
     public function new(Request $request): Response
     {
-        $form = $this->createFormBuilder()
+        $task = new Task();
+        $form = $this->createFormBuilder($task)
                 ->add('name', TextType::class)
                 ->add('description', TextareaType::class)
-                ->getForm()
-                ->createView();
+                ->add('Salvar', SubmitType::class)
+                ->getForm();
 
-        if ($request->isMethod("POST") && $this->validToken($_SERVER['CREATE_TASK'], $request)) {
-            $task = new Task();
-            $task->setName($request->request->get('name'));
-            $task->setDescription($request->request->get('description'));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $task = $form->getData();
             $task->setScheduling(new \DateTime());
 
             $entityManager = $this->getDoctrine()->getManager();
@@ -62,7 +64,7 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/new.html.twig', [
-            "form" => $form
+            "form" => $form->createView()
         ]);
     }
 
